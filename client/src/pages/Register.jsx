075136e957504cbc45fcd2220320223bdb941a70
J0hn316 <jcordell316@gmail.com>
2024-02-17 21:04:54 -0500

    update frontend code

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,13 +1,27 @@
 import { Form, redirect, useNavigation, Link } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
 import { Logo, FormRow } from '../components';
+import customFetch from '../utils/customFetch.js';
+import { toast } from 'react-toastify';
 
-export const action = async (data) => {
-  console.log(data);
-  return null;
+export const action = async ({ request }) => {
+  // the redirect import only works with  action functions line 14
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  try {
+    await customFetch.post('/auth/register', data);
+    toast.success('Registration successful!');
+    return redirect('/login');
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+    return error;
+  }
 };
 
 const Register = () => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
+
   return (
     <Wrapper>
       <Form method="post" className="form">
@@ -24,8 +38,8 @@ const Register = () => {
         <FormRow type="email" name="email" defaultValue="John@gmail.com" />
         <FormRow type="password" name="password" />
 
-        <button type="submit" className="btn btn-block">
-          submit
+        <button type="submit" className="btn btn-block" disabled={isSubmitting}>
+          {isSubmitting ? 'submitting...' : 'submit'}
         </button>
         <p>
           Already a member?
