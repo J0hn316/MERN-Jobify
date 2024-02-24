3465f5421508beede705b9459b4ad4479cc5e3ec
J0hn316 <jcordell316@gmail.com>
2024-02-24 12:49:39 -0500

    fix front-end

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ cloudinary.config({
   api_secret: process.env.CLOUD_API_SECRET,
 });
 
-app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.resolve(__dirname, './client/dist')));
 
 app.use(cookieParser());
 app.use(express.json());
@@ -54,7 +54,7 @@ app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './public', 'index.html'));
+  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
 });
 
 // This route needs to be at the end of all routes
